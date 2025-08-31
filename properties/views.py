45a193677dd2f3851769adb185fc1851ffa2445e
James Moseti <moseti.james4@gmail.com>
2025-08-31 05:30:16 +0300

    feat: add low-level caching for Property queryset with Redis

--- properties/views.py
+++ properties/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-from .models import Property
+from .utils import get_all_properties
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = list(Property.objects.all().values("id", "title", "description", "price", "location", "created_at"))
+    properties = get_all_properties()
     return JsonResponse({"data": properties})
